a16da82eea6d0958fd5b64102bce9b3528db698e
oliverjam <oliverjamesphillips@gmail.com>
2019-04-14 15:03:45 +0100

    Add food and level increase
    Fix direction inconsistency bug

--- index.js
+++ index.js
@@ -39,56 +39,46 @@ const opposite = {
   up: "down",
   right: "left",
   down: "up",
-  left: "right"
+  left: "right",
 };
 
-function init(initState) {
-  const INITIAL_COORDS = Array.from({ length: 10 }, (_, i) => [2 + i, 4]);
+function placeFood(size) {
+  return [
+    Math.floor(Math.random() * size) + 1,
+    Math.floor(Math.random() * size) + 1,
+  ];
+}
+
+function init({ size = 32 } = {}) {
+  const INITIAL_COORDS = Array.from({ length: size }, (_, i) => [2 + i, 4]);
+  const INITIAL_FOOD_COORDS = placeFood(size);
   return {
     game: "initial",
     dir: "right",
-    coords: INITIAL_COORDS
+    coords: INITIAL_COORDS,
+    food: INITIAL_FOOD_COORDS,
+    size,
+    level: 1,
   };
 }
 
 function reducer(state, action) {
-  console.log(action);
   switch (action.type) {
     case "RESET":
-      return init();
+      return { ...init(), game: "running" };
     case "START":
-      console.log({ ...state, game: "running" });
       return { ...state, game: "running" };
     case "PAUSE":
       return { ...state, game: "paused" };
     case "TURN":
       return {
         ...state,
-        dir:
-          action.payload.dir === opposite[state.dir]
-            ? state.dir
-            : action.payload.dir
+        nextDir: [action.newDir, opposite[action.newDir]].includes(state.dir)
+          ? state.dir
+          : action.newDir,
       };
-    case "TICK":
-      return {
-        game: action.payload.clash ? "dead" : "running",
-        dir: state.dir,
-        coords: action.payload.clash ? state.coords : action.payload.coords
-      };
-    default:
-      return state;
-  }
-}
-
-function Board({ size = 32, children }) {
-  const [state, dispatch] = Hooks.useReducer(reducer, undefined, init);
-  const { game, dir, coords } = state;
-  console.log(state);
-  // const [coords, setCoords] = Hooks.useState(INITIAL_COORDS);
-  // const [dir, setDir] = Hooks.useState("right");
-  // const [game, setGame] = Hooks.useState("running");
-  useInterval(200, () => {
-    if (game === "running") {
+    case "TICK": {
+      const { dir, nextDir, coords, size, food, level } = state;
       const max = size + 1; // css grid isn't 0 indexed
       const forward = ["down", "right"].includes(dir);
       const vertical = ["up", "down"].includes(dir);
@@ -99,79 +89,134 @@ function Board({ size = 32, children }) {
       const clash = coords.some(
         ([x, y]) => x === newHead[0] && y === newHead[1]
       );
-      const newCoords = clash ? coords : coords.concat([newHead]).slice(1); // only move if there was no clash
-      // if (clash) setGame("finished");
-      // setCoords(clash ? coords : newCoords);
-      dispatch({ type: "TICK", payload: { coords: newCoords, clash } });
+      const foodEaten = newHead[0] === food[0] && newHead[1] === food[1];
+      const newCoords = coords.concat([newHead]).slice(foodEaten ? 0 : 1); // only move if there was no clash
+      const newFoodCoords = foodEaten ? placeFood(size) : food;
+      console.log(newFoodCoords);
+      return {
+        ...state,
+        game: clash ? "dead" : "running",
+        dir: nextDir || dir,
+        coords: clash ? coords : newCoords,
+        food: newFoodCoords,
+        level: foodEaten ? level + 1 : level,
+      };
     }
-  });
-  // Hooks.useLayoutEffect(() => {
-  //   console.log(dir);
-  // }, [dir]);
+    default:
+      return state;
+  }
+}
+
+function Board({ size = 32, children }) {
+  const nextDir = Hooks.useRef(null);
+  const [state, dispatch] = Hooks.useReducer(reducer, { size }, init);
+  const { game, dir, coords, food, level } = state;
+
+  Hooks.useEffect(() => {
+    if (game !== "running") return;
+    const interval = setInterval(() => {
+      dispatch({ type: "TICK", nextDir: nextDir.current });
+    }, Math.max(180 - level * 10, 40));
+    return () => clearInterval(interval);
+  }, [game, level]);
+
   Hooks.useEffect(() => {
     function handleKeyDown(event) {
-      switch (event.key) {
-        case "ArrowUp":
-          event.preventDefault();
-          // return setDir(prev => (prev === "down" ? "down" : "up"));
-          return dispatch({ type: "TURN", payload: { dir: "up" } });
-        case "ArrowRight":
-          event.preventDefault();
-          // return setDir(prev => (prev === "left" ? "left" : "right"));
-          return dispatch({ type: "TURN", payload: { dir: "right" } });
-        case "ArrowDown":
-          event.preventDefault();
-          // return setDir(prev => (prev === "up" ? "up" : "down"));
-          return dispatch({ type: "TURN", payload: { dir: "down" } });
-        case "ArrowLeft":
-          event.preventDefault();
-          // return setDir(prev => (prev === "right" ? "right" : "left"));
-          return dispatch({ type: "TURN", payload: { dir: "left" } });
-        default:
-          return;
+      if (event.key.includes("Arrow")) {
+        event.preventDefault(); // stops the page scrolling
+        const newDir = event.key.replace("Arrow", "").toLowerCase(); // e.g. "ArrowUp" -> "up"
+        dispatch({ type: "TURN", newDir });
       }
     }
     document.addEventListener("keydown", handleKeyDown);
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, []);
+
   return html`
-    <div>
-      <mark>${dir}</mark>
-      <br />
-      <mark>${game}</mark>
-      <br />
-      <button
-        onClick=${() => dispatch({ type: game === "dead" ? "RESET" : "START" })}
-      >
-        Start
-      </button>
-      <ul
+    <div
+      style=${{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        padding: "2rem",
+        gap: "2rem",
+      }}
+    >
+      <div
         style=${{
           maxWidth: "50rem",
           maxHeight: "50rem",
-          width: "90vw",
-          height: "90vw",
-          display: "grid",
-          gridTemplateColumns: `repeat(${size}, 1fr)`,
-          gridTemplateRows: `repeat(${size}, 1fr)`,
-          outline: "1px solid"
+          width: "80vw",
+          height: "80vw",
+          maxWidth: "90vh",
+          maxHeight: "90vh",
+          position: "relative",
+          border: "0.5rem solid",
+          // backgroundColor: `hsl(220, 10%, ${game === "dead" ? "90%" : "98%"})`,
         }}
       >
-        ${coords.map(
-          ([x, y], i) =>
-            html`
+        <ul
+          style=${{
+            width: "100%",
+            height: "100%",
+            display: "grid",
+            gridTemplateColumns: `repeat(${size}, 1fr)`,
+            gridTemplateRows: `repeat(${size}, 1fr)`,
+          }}
+        >
+          ${coords.map(
+            ([x, y], i) => html`
               <li
                 key=${i}
                 style=${{
                   gridRowStart: y,
                   gridColumnStart: x,
                   backgroundColor:
-                    i === coords.length - 1 && game === "dead" ? "red" : "#000"
+                    i === coords.length - 1 && game === "dead" ? "red" : "#000",
+                  // borderRadius: isFood ? "50%" : 0,
                 }}
               ></li>
             `
-        )}
-      </ul>
+          )}
+          <li
+            key="food"
+            style=${{
+              gridRowStart: food[1],
+              gridColumnStart: food[0],
+              borderRadius: "50%",
+              backgroundColor: "blue",
+            }}
+          ></li>
+        </ul>
+        ${["dead", "initial"].includes(game) &&
+          html`
+            <button
+              style=${{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                border: 0,
+                backgroundColor: `hsla(${
+                  game === "initial" ? "190" : "0"
+                }, 50%, 40%, 0.5)`,
+                fontSize: "3rem",
+                fontWeight: "900",
+                cursor: "pointer",
+                zIndex: 10,
+              }}
+              onClick=${() =>
+                dispatch({ type: game === "initial" ? "START" : "RESET" })}
+            >
+              ${game === "initial" ? "Start" : "Restart"}
+            </button>
+          `}
+      </div>
+      <div>
+        <form>
+          <input />
+        </form>
+      </div>
     </div>
   `;
 }
